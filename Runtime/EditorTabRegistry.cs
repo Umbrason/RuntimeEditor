71ed7660f27be984758baa6d25f3482882ec708f
Umbrason <FabioKolligs@outlook.de>
2021-10-26 23:03:00 +0200

    Update EditorTabRegistry.cs

--- Runtime/EditorTabRegistry.cs
+++ Runtime/EditorTabRegistry.cs
@@ -7,27 +7,27 @@ public static class EditorTabRegistry
 {
     private static Dictionary<string, (GameObject, Sprite)> EditorInfoRegistry = new Dictionary<string, (GameObject, Sprite)>();
 
-    public static void Register(string identifier, GameObject prefab, Sprite icon) => EditorInfoRegistry[identifier] = (prefab, icon);
+    public static void Register(string identifier, GameObject prefab, Sprite icon) => EditorInfoRegistry[identifier.ToLower()] = (prefab, icon);
 
     public static Sprite GetIcon(string identifier)
     {
-        if (!EditorInfoRegistry.ContainsKey(identifier))
+        if (!EditorInfoRegistry.ContainsKey(identifier.ToLower()))
             return null;
-        return EditorInfoRegistry[identifier].Item2;
+        return EditorInfoRegistry[identifier.ToLower()].Item2;
     }
 
     public static GameObject GetPrefab(string identifier)
     {
-        if (!EditorInfoRegistry.ContainsKey(identifier))
+        if (!EditorInfoRegistry.ContainsKey(identifier.ToLower()))
             return null;
-        return EditorInfoRegistry[identifier].Item1;
+        return EditorInfoRegistry[identifier.ToLower()].Item1;
     }
 
     public static (string, Sprite) GetDescriptor(string identifier)
     {
-        if (!EditorInfoRegistry.ContainsKey(identifier))
+        if (!EditorInfoRegistry.ContainsKey(identifier.ToLower()))
             return ("null", Sprite.Create(Texture2D.blackTexture, new Rect(0, 0, 1, 1), Vector2.one * .5f));
-        var val = EditorInfoRegistry[identifier];
+        var val = EditorInfoRegistry[identifier.ToLower()];
         return (identifier, val.Item2);
     }
 }
